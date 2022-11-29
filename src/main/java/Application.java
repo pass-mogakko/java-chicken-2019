b9aca79760f3d8c26e6dab5fcce7170970027d43
Isu Shin <isu010614@naver.com>
2022-11-29 17:42:49 +0900

    feat: 테이블 번호를 입력받는 기능

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,38 +1,64 @@
-import domain.Menu;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
 import java.util.List;
 
-import static view.InputView.inputCommand;
+import static domain.Command.isExitCommand;
+import static domain.Command.readInputCommand;
 
 
 public class Application {
     static final List<Table> tables = TableRepository.tables();
     static final List<Menu> menus = MenuRepository.menus();
+    static final List<Payment> payments = PaymentRepository.payments();
 
     public static void main(String[] args) {
-        final int tableNumber = InputView.inputTableNumber();
+        int command = readCommand();
 
-
+        while (!isExitCommand(command)) {
+            runPOS();
+            command = readCommand();
+        }
         OutputView.printMenus(menus);
     }
 
-    private void printInitSetting() {
-        readCommand();
-        OutputView.printTables(tables);
+    private static void runPOS() {
+        printInitSetting();
+        //TODO 1, 2번의 경우
     }
 
-    private int readCommand() {
+    private static void printInitSetting() {
+        OutputView.printTables(tables);
+        readTableNumber();
+    }
+
+    private static int readCommand() {
         OutputView.printMainView();
         try {
-            return inputCommand();
+            int command = InputView.inputCommand();
+            return readInputCommand(command);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return readCommand();
+        }
+    }
+
+    private static int readTableNumber() {
+        try {
+            final int tableNumber = InputView.inputTableNumber();
+            validateTableNumber(tableNumber);
+            return tableNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readTableNumber();
+        }
+    }
+
+    public static void validateTableNumber(final int number) {
+        if (tables.stream()
+                .noneMatch(table -> table.getTableNumber() == number)) {
+            throw new IllegalArgumentException("[ERROR] 허용된 테이블이 아닙니다.");
         }
     }
 }
