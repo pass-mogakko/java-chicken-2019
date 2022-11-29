e84154823145366b5134569403810adc574d183f
kjb95 <topmedia44@gmail.com>
2022-11-29 15:00:09 +0900

    feat: 메인화면 출력하기

--- src/main/java/controller/MainScreenController.java
+++ src/main/java/controller/MainScreenController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import domain.Table;
+import domain.TableRepository;
+import java.util.List;
+import view.OutputView;
+
 public class MainScreenController {
 
     private static final MainScreenController mainController = new MainScreenController();
@@ -13,6 +18,12 @@ public class MainScreenController {
     }
 
     public void run() {
+        final List<Table> tables = TableRepository.tables();
+        OutputView.printTables(tables);
 
+        //        final int tableNumber = InputView.inputTableNumber();
+        //
+        //        final List<Menu> menus = MenuRepository.menus();
+        //        OutputView.printMenus(menus);
     }
 }
