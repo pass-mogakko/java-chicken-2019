2b2d9bb75cb544e5e7ed500e9dcae4d5f7d53913
송민진 <m_6595@naver.com>
2022-11-29 16:42:30 +0900

    refactor(chicken): InputView의 메시지를 상수로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,8 +3,8 @@ package view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
     private static final String REQUEST_COMMAND_MESSAGE = "수행할 작업을 선택하세요. (1. 주문등록 / 2. 결제하기 / 3. 프로그램 종료)";
+    private static final String REQUEST_TABLE_NUMBER_MESSAGE = "## 주문할 테이블을 선택하세요.";
 
     public static String inputCommand() {
         System.out.println(REQUEST_COMMAND_MESSAGE);
@@ -12,7 +12,13 @@ public class InputView {
     }
 
     public static String inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println();
+        System.out.println(REQUEST_TABLE_NUMBER_MESSAGE);
         return Console.readLine();
     }
+
+//    public static String inputMenuNumber() {
+//        System.out.println();
+//        return Console.readLine();
+//    }
 }
