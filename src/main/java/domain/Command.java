634f59a652afa6b2fab77aabf3112596045b8dcc
Isu Shin <isu010614@naver.com>
2022-11-29 16:23:34 +0900

    feat: 입력 예외처리 구현

--- src/main/java/domain/Command.java
+++ src/main/java/domain/Command.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Command {
     ORDER_REGISTRATION(1),
     PAYMENT(2),
@@ -9,5 +11,23 @@ public enum Command {
 
     Command(final int command) {
         this.command = command;
+    }
+
+    public static int readInputCommand(final int command){
+        validateCommand(command);
+        return command;
+    }
+
+    private static void validateCommand(final int command) {
+        if (!Arrays.stream(Command.values())
+                .findAny()
+                .equals(command)
+        ) {
+            throw new IllegalArgumentException("[ERROR] 1,2,3만 입력할 수 있습니다.");
+        }
+    }
+
+    public int getCommand(){
+        return this.command;
     }
 }
