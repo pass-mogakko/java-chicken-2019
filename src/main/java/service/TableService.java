6a05e56b4b6ae3eb928015a47731877bc2045fa9
yoondgu <doyou2ing@naver.com>
2022-11-30 16:36:12 +0900

    :sparkles:feat(Domain-1): 모든 테이블 목록 정보를 조회하는 기능 구현

--- src/main/java/service/TableService.java
+++ src/main/java/service/TableService.java
@@ -1,4 +1,13 @@
 package service;
 
+import domain.Table;
+import domain.TableRepository;
+
+import java.util.List;
+
 public class TableService {
+
+    public List<Table> getAllTables() {
+        return TableRepository.tables();
+    }
 }
