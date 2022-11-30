92a223a64ae390284bc951042543682346cef112
송민진 <m_6595@naver.com>
2022-11-30 11:40:28 +0900

    feat(chicken): 주문 내역 불러오기

--- src/main/java/domain/order/Order.java
+++ src/main/java/domain/order/Order.java
@@ -3,14 +3,23 @@ package domain.order;
 import domain.menu.Menu;
 import domain.table.Table;
 
+import java.util.Optional;
+
 public class Order {
-    private Table table;
+    private Optional<Table> table;
     private Menu menu;
     private int count;
 
-    public Order(Table table, Menu menu, int count) {
+    public Order(Optional<Table> table, Menu menu, int count) {
         this.table = table;
         this.menu = menu;
         this.count = count;
+    }
+
+    public boolean isEqualTable(Optional<Table> table) {
+        if (this.table.equals(table)) {
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/domain/order/OrderService.java
+++ src/main/java/domain/order/OrderService.java
@@ -5,11 +5,19 @@ import domain.menu.MenuRepository;
 import domain.table.Table;
 import domain.table.TableRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class OrderService {
 
     public static void createOrder(int tableNumber, int menuNumber, int count) {
-        Table table = TableRepository.findByNumber(tableNumber);
+        Optional<Table> table = TableRepository.findByNumber(tableNumber);
         Menu menu = MenuRepository.findByNumber(menuNumber);
         OrderRepository.save(table, menu, count);
+    }
+
+    public static List<Order> showOrders(int tableNumber) {
+        List<Order> orders = OrderRepository.findAllByTableNumber(tableNumber);
+        return orders;
     }
 }
